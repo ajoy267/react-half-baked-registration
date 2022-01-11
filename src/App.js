7d413ce606ff11fcd3f1e66619206b83cd13e49a
ajoy267 <andrew.joy12@hotmail.com>
2022-01-10 17:45:14 -0800

    added new route so it shows something when logged in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
+import { getUser } from './services/users';
 import Auth from './views/Auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
           <Route path="/" exact>
-            <Auth />
+            {currentUser && (
+              <div>
+                <h2>You are Signed In</h2>
+              </div>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
           </Route>
         </Switch>
       </BrowserRouter>
